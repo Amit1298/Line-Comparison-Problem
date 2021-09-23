5399512a968113c487620c9374c0834d8c12054f
Amit <amitkr24031995@gmail.com>
2021-09-23 08:25:37 +0530

    Adding line camparing all

--- Line Comparison Problem/Program.cs
+++ Line Comparison Problem/Program.cs
@@ -8,9 +8,11 @@ namespace Line_Comparison_Problem
         {
             Cartesian_system LenghtLine = new Cartesian_system();
             CompareLineLength lineCompare = new CompareLineLength();
+            comparingLineSize compareSize = new comparingLineSize();
             Console.WriteLine("Welcome to Line Comparison Problem");
-           // LenghtLine.Cartesion();
-            lineCompare.compareLine();
+            // LenghtLine.Cartesion();
+            //lineCompare.compareLine();
+            compareSize.lineSize();
             Console.ReadLine();
         }
     }
